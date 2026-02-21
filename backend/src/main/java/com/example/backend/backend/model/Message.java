1d05cac0d121d1a3fc6228cc7b252ce2561f275b
Leo Suasnabar <Darkltata@gmail.com>
2026-02-20 22:08:08 -0500

    algo se me esta pasando realmente el sistema funciona la api funciona pero al momento de mandar el mensaje no me llega

--- backend/src/main/java/com/example/backend/backend/model/Message.java
+++ backend/src/main/java/com/example/backend/backend/model/Message.java
@@ -14,11 +14,37 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
- 
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
     @Column(name = "email", nullable = false, length = 50)
     private String email;
-    
+
     @Column(name = "content", nullable = false, length = 100)
     private String content;
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
